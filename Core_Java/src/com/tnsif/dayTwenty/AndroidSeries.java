6a984bf38f6d9e6ab370311ebfc31cb7397e5cb2
CC-57 <CC-57@172.16.12.25>
2024-06-26 16:09:35 +0530

    commit

---
+++ Core_Java/src/com/tnsif/dayTwenty/AndroidSeries.java
@@ -0,0 +1,17 @@
+package com.tnsif.dayTwenty;
+@SmartTV(os="Android", price=80000)
+public class AndroidSeries {
+String model;
+int screenSize;
+
+public AndroidSeries(String model,int screenSize) {
+	this.model =model;
+	this.screenSize=screenSize;
+
+}
+
+@Override
+public String toString() {
+	return "AndroidSeries [model=" + model + ", screenSize=" + screenSize + "]";
+}
+}
